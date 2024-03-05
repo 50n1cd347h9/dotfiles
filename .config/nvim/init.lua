8ca0fd45a49d906df885f0c4c2e87c781c70bd04
root <root@ubuntu.192.168.0.1>
2024-03-05 07:17:59 +0000

    root prompt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,45 +12,27 @@ vim.keymap.set('i', '\"', '\"\"<LEFT>')
 
 vim.keymap.set('n', '<C-n>', ':Fern . -reveal=% -drawer -toggle -width=40<CR>')
 
+vim.keymap.set('t', 'jj', '<C-\\><C-n>')
+vim.keymap.set('t', '<C-w>N', '<C-\\><C-n>')
+vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>')
+
 vim.opt.relativenumber = true
 vim.opt.syntax = 'enable'
 vim.cmd('colorscheme hybrid')
 vim.opt.clipboard = "unnamedplus"
+vim.opt.shell = 'bash'
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
-
---tnoremap <C-w>N <C-\><C-n>
---tnoremap jj <C-\><C-n>
---tnoremap <C-w> <C-\><C-n><C-w>
---
---nnoremap sh <C-w>s<CR><C-w>w<CR>:term<CR>:set norelativenumber<CR>i
---
---set nowritebackup
---set nobackup
---set shell=bash
---set noswapfile
---set autoindent 
---set tabstop=4
---set shiftwidth=4
-
-
-
---function! OpenFern()
---	execute "Fern . -reveal=% -drawer -toggle -width=40\<CR>\<Esc>"
---endfunction
---
---function! OpenTerminal()
---	execute "normal! \<C-w>s\<C-w>w:term\<CR>:resize 10\<CR>:set norelativenumber\<CR>"
---endfunction
---
---augroup vimrc
---	autocmd!
---	autocmd VimEnter * call OpenTerminal() 
---	autocmd VimEnter * nested call OpenFern()
---	"autocmd bufread,bufnewfile * nested call OpenFern()
---augroup END
+vim.api.nvim_create_augroup('vimrc', { clear = true })
+-- vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+-- 	pattern = {"*"},
+-- 	command = "Fern . -reveal=% -drawer -toggle -width=40\\<CR>\\<Esc>"
+-- })
 
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = {"c", "python", "typescript", "vim", "yaml", "rust"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = {"c", "python", "typescript", "vim", "yaml", "rust", "lua"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = true,
 	highlight = {
 		enable = true,              -- false will disable the whole extension
