db654159471e887b2d24f6fb7b48f1bcbd45a89d
s0n1cd347h9 <8yzuie9g@duck.com>
2024-09-13 05:51:15 +0900

    aaa

--- .config/sway/set_windows.sh
+++ .config/sway/set_windows.sh
@@ -2,7 +2,4 @@ swaymsg "workspace 1"
 swaymsg 'exec --no-startup-id brave-browser'
     
 swaymsg "workspace 2"
-swaymsg 'exec --no-startup-id $HOME/binaryninja/binaryninja'
-
-swaymsg "workspace 3"
 swaymsg 'exec --no-startup-id wezterm'
