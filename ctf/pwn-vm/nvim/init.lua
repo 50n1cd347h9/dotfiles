fd7a86efce375ba7ca0ea4ac951348995cfa6eb0
fdjkaf <hoge@hog4e.com>
2025-12-31 04:32:03 +0900

    nvim plugins settings on pwn-vm

--- ctf/pwn-vm/nvim/init.lua
+++ ctf/pwn-vm/nvim/init.lua
@@ -7,9 +7,8 @@ vim.keymap.set('i', '\"', '\"\"<LEFT>')
 vim.keymap.set('t', '<C-w>N', '<C-\\><C-n>')
 vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>')
 
+-- vim.opt.clipboard = "unnamedplus"
 vim.opt.relativenumber = true
-vim.api.nvim_set_hl(0, 'LineNr', { fg = "white"})
-
 vim.opt.syntax = 'enable'
 vim.opt.shell = 'bash'
 vim.opt.autoindent = true
@@ -17,10 +16,25 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 vim.opt.termguicolors = true
+require("tokyonight").setup({
+	terminal_colors = true,
+    transparent = true,
+	comments = { italic = true },
+    keywords = { italic = true },
 
-vim.cmd [[
-highlight Normal guibg=none
-highlight NonText guibg=none
-highlight Normal ctermbg=none
-highlight NonText ctermbg=none
-]]
+    styles = {
+        sidebars = "dark",
+        floats = "transparent",
+    },
+})
+vim.cmd('colorscheme tokyonight')
+
+-- TreeSitter setting
+--require'nvim-treesitter.configs'.setup {
+--	ensure_installed = {"c", "python", "zig"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+--	sync_install = true,
+--	highlight = {
+--		enable = true,              -- false will disable the whole extension
+--		disable = {},  -- list of language that will be disabled
+--	},
+--}
